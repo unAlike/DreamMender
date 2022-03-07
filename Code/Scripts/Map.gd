6f3ab563ecae58cbe3b87a231eb7ac44e622d8ff
Tyler <tsvihovec01@gmail.com>
2022-03-07 12:33:45 -0600

    Added Main Menu
    Simple functionality so far, will be improved upon later.

--- Code/Scripts/Map.gd
+++ Code/Scripts/Map.gd
@@ -1,14 +1,9 @@
 extends StaticBody2D
 
+var load_saved_game = false
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$CollisionShape2D/Polygon2D.polygon = $CollisionShape2D.polygon
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func save():
+	pass
