a779d8141cc18926b04f6e13b08d430397c24e3c
Tyler <tsvihovec01@gmail.com>
2022-04-04 16:24:53 -0500

    We got a working boat!

--- Code/Scripts/Boat.gd
+++ Code/Scripts/Boat.gd
@@ -4,4 +4,17 @@ func _ready():
 	pass
 
 func _on_CollisionDetector_body_entered(body):
+	print("Hello")
+	$AnimationPlayer.current_animation = "Start"
 	$AnimationPlayer.play()
+
+
+func onEntered():
+	print("Worked")
+	$AnimationPlayer.play()
+
+
+func _on_CollisionDetector_area_entered(area):
+	print("Worked")
+	$AnimationPlayer.play()
+	pass # Replace with function body.
