66e25e2686efae91b781a3b35cb4dcae4f4a7a6a
Tyler <tsvihovec01@gmail.com>
2022-04-06 17:27:21 -0500

    Added credits scene
    Also removed load feature on main menu and replaced with button to show credits scene

--- Code/Scripts/StartScreen.gd
+++ Code/Scripts/StartScreen.gd
@@ -6,16 +6,12 @@ func _ready():
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://Scenes/MainLevel.tscn")
 
-func _on_LoadButton_pressed():
-	var next_level_resource = load("res://Scenes/MainLevel.tscn")
-	var next_level = next_level_resource.instance()
-	next_level.load_saved_game = true
-	get_tree().root.call_deferred("add_child", next_level)
-	queue_free()
-
 func _on_OptionsButton_pressed():
 	var options = load("res://Scenes/OptionsScreen.tscn").instance()
 	get_tree().current_scene.add_child(options)
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+func _on_CreditsButton_pressed():
+	get_tree().change_scene("res://Scenes/EndGameScreen.tscn")
