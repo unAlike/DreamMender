d4fed51668eb2fbcced15f6c7af3ad5a0ae76fba
Devlyn7299 <nwokeukud2011@my.uwstout.edu>
2022-04-08 04:17:00 -0500

    Shakes Less

--- Code/TempScript.gd
+++ Code/TempScript.gd
@@ -4,15 +4,22 @@ onready var Player = owner.get_node("Player")
 onready var Stop = get_parent().get_node("Stop")
 var Radius = 100
 var Sail = false
-var Speed = 10
+var Speed = 500
+var Velocity = Vector2(1,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	if Player.global_position.distance_to($Start.global_position) <= Radius:
 		Sail = true
-	if Sail:
-		global_position = lerp(global_position, Stop.global_position, delta/Speed)
+	if Sail and position <= Stop.position:
+		move_and_collide(Velocity.normalized() * delta * Speed)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	if Player.global_position.distance_to($Start.global_position) <= Radius:
+#		Sail = true
+#	if Sail:
+#		global_position = lerp(global_position, Stop.global_position, delta/Speed)
