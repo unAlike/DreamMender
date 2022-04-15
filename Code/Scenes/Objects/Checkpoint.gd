1b3670384b4c39101b825b4ff1cf21e6e8835853
Tyler <tsvihovec01@gmail.com>
2022-04-15 17:26:26 -0500

    Fixed boat

--- Code/Scenes/Objects/Checkpoint.gd
+++ Code/Scenes/Objects/Checkpoint.gd
@@ -1,9 +1,13 @@
 extends Area2D
 
+var checkpoint_on = preload("res://Art/Misc/DW_Checkpoint_On.png")
+onready var checkpoint_sprite = get_node("Sprite")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 func _on_Checkpoint_body_entered(body):
+	checkpoint_sprite.set_texture(checkpoint_on)
 	Checkpoint.last_position = global_position
--- Code/Scripts/Boat(New).gd
+++ Code/Scripts/Boat(New).gd
@@ -1,6 +1,13 @@
 extends KinematicBody2D
 
-var lerpVal = 1
+var lerpVal = 0
+var moving = false
+
+func _process(delta):
+	if moving:
+		lerpVal += delta / 1000
+		global_position = lerp( get_node("fromPos").global_position, get_node("toPos").global_position, lerpVal)
 
 func _on_CollisionDetector_body_entered(body):
-	global_position = lerp( get_node("fromPos").global_position, get_node("toPos").global_position, lerpVal)
+	if body.name == "Player":
+		moving = true
