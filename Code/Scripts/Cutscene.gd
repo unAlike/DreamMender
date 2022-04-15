fd66f6c1a94a43781939bdf00105f8ba6f5cdb6d
Devlyn7299 <nwokeukud2011@my.uwstout.edu>
2022-04-15 17:37:46 -0500

    All My Changes

---
+++ Code/Scripts/Cutscene.gd
@@ -0,0 +1,18 @@
+extends Sprite
+
+var Fade = false
+export var FadeSpeed = .025
+export var Radius = 100
+
+onready var Player = get_tree().current_scene.get_node("Player")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Player.get_global_position().distance_to(self.get_global_position()) < Radius:
+		Fade = true
+	if Fade:
+		modulate.a = lerp(modulate.a, 0, FadeSpeed)
--- Code/Scripts/Dialogue.gd
+++ Code/Scripts/Dialogue.gd
@@ -4,11 +4,12 @@ onready var Box = get_node("Background")
 onready var Dialogue = get_node("Background/RichTextLabel")
 onready var Duration = get_node("Timer")
 onready var Bubble = get_node("ENTER")
-export var Radius = 100
+export var Radius = 200
 export(PoolStringArray) var Text
 export(PoolStringArray) var Next
 var InRadius = false
 var Interacting = false
+var Interactable = true
 var At = 0
 onready var Player = get_tree().current_scene.get_node("Player")
 onready var NPC = get_parent().get_node("NPC")
@@ -20,29 +21,39 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Player.get_global_position().distance_to(NPC.get_global_position()) < Radius:
-		#print("In Radius")
-		InRadius = true
-	else:
-		InRadius = false
-	
-	if InRadius and !Interacting:
-		Bubble.visible = true
-	else:
-		Bubble.visible = false
-	
-	if Input.is_action_just_pressed("ui_accept") and InRadius:
-		if !Interacting:
-			Interacting = true
-			Message()
-			At += 1
-		elif Interacting and At == Text.size():
+	if Interactable:
+		if Player.get_global_position().distance_to(NPC.get_global_position()) < Radius:
+			#print("In Radius")
+			InRadius = true
+		else:
+			InRadius = false
+		
+		if InRadius and !Interacting:
+			Bubble.visible = true
+		else:
+			Bubble.visible = false
+		
+		if Input.is_action_just_pressed("ui_accept") and InRadius:
+			if !Interacting:
+				Interacting = true
+				Message()
+				At += 1
+			elif Interacting and At == Text.size():
+				Box.visible = false
+				Interacting = false
+				At = 0
+			elif Interacting and At != Text.size():
+				Message()
+				At += 1
+		
+		# To be deleted when movement is disabled
+		if Input.is_action_just_pressed("ui_accept") and !InRadius and Interacting:
 			Box.visible = false
 			Interacting = false
-			At = 0
-		elif Interacting and At != Text.size():
-			Message()
-			At += 1
+	
+	elif Input.is_action_just_pressed("ui_accept") and !Interactable:
+		Box.visible = false
+		Interacting = false
 
 func Message():
 	pass
