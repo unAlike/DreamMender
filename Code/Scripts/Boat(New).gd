95517f322d952f9537230f7e90e2456dc2a1c4d3
unAlike <Ryan.Ungerleider@gmail.com>
2022-04-15 17:34:59 -0500

    Merged

--- Code/Scripts/Boat(New).gd
+++ Code/Scripts/Boat(New).gd
@@ -5,7 +5,8 @@ var moving = false
 
 func _process(delta):
 	if moving:
-		lerpVal += delta / 1000
+		if lerpVal<1:
+			lerpVal += delta / 1000
 		global_position = lerp( get_node("fromPos").global_position, get_node("toPos").global_position, lerpVal)
 
 func _on_CollisionDetector_body_entered(body):
