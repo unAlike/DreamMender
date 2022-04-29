5f1d08e1e4b00fa8723d55a9078e8f865d1fc241
Devlyn7299 <nwokeukud2011@my.uwstout.edu>
2022-04-28 23:34:54 -0500

    More Art

--- Code/Scripts/Shadow5.gd
+++ Code/Scripts/Shadow5.gd
@@ -1,7 +1,16 @@
 extends "res://Scripts/Dialogue.gd"
 
 var Fade = false
+var Fade2 = false
+var Fade3 = false
 export var FadeSpeed = .025
+
+onready var NPC1 = get_parent().get_node("NPC")
+onready var NPC2 = get_parent().get_node("NPC2")
+onready var NPC3 = get_parent().get_node("NPC3")
+onready var NPC4 = get_parent().get_node("NPC4")
+onready var NPC5 = get_parent().get_node("NPC5")
+onready var NPC6 = get_parent().get_node("NPC6")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,7 +19,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Fade:
-		get_parent().modulate.a = lerp(get_parent().modulate.a, 0, FadeSpeed)
+		NPC1.modulate.a = lerp(NPC1.modulate.a, 0, FadeSpeed)
+		NPC4.modulate.a = lerp(NPC4.modulate.a, 0, FadeSpeed)
+	if Fade2:
+		NPC2.modulate.a = lerp(NPC2.modulate.a, 0, FadeSpeed)
+		NPC5.modulate.a = lerp(NPC5.modulate.a, 0, FadeSpeed)
+	if Fade3:
+		NPC3.modulate.a = lerp(NPC3.modulate.a, 0, FadeSpeed)
+		NPC6.modulate.a = lerp(NPC6.modulate.a, 0, FadeSpeed)
 
 func Message():
 	if Tracker == 1:
@@ -23,6 +39,10 @@ func Message():
 	Duration.start()
 	if At == Text.size() - 1:
 		Tracker += 1
-		if Tracker == 3:
+		if Tracker == 1:
 			Fade = true
+		elif Tracker == 2:
+			Fade2 = true
+		elif Tracker == 3:
+			Fade3 = true
 			Interactable = false
