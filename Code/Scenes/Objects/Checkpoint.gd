0955099f664676e7c2f15327161f514312e35739
unAlike <Ryan.Ungerleider@gmail.com>
2022-04-30 00:32:05 -0500

    JUICE

--- Code/Scenes/Objects/Checkpoint.gd
+++ Code/Scenes/Objects/Checkpoint.gd
@@ -3,11 +3,18 @@ extends Area2D
 var activated = false
 var checkpoint_on = preload("res://Art/Misc/DW_Checkpoint_On.png")
 onready var checkpoint_sprite = get_node("Sprite")
+var spinTime = 0
+var bobTime = 0
+export var spinSpeed = 0.5
+export var bobDistance = 5
+export var bobSpeed = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+func _process(delta):
+	spinTime += delta*spinSpeed
+	$Sprite.rotation = sin(spinTime)/5
+	bobTime += delta*bobSpeed
+	$Sprite.position.y = sin(bobTime)*bobDistance
+	$Sprite.position.y = $Sprite.position.y
 
 func _on_Checkpoint_body_entered(body):
 	if body.name == "Player":
@@ -16,4 +23,5 @@ func _on_Checkpoint_body_entered(body):
 			Checkpoint.last_position = global_position
 			$Hit.play()
 			activated = true
+			$CPUParticles2D.emitting = false
 		
--- Code/Scripts/Collectable.gd
+++ Code/Scripts/Collectable.gd
@@ -1,7 +1,14 @@
 extends Node2D
 
 signal get_button
+var bobTime = 0
+export var bobDistance = 10
+export var bobSpeed = 5
 
+func _process(delta):
+	bobTime += delta*bobSpeed
+	$ButtonSprite.position.y = sin(bobTime)*bobDistance
+	$ButtonArea2D.position.y = $ButtonSprite.position.y
 func _on_ButtonArea2D_body_entered(body):
 	if body.name == "Player":
 		get_tree().get_current_scene().get_node("Player/Progress/Control").Collected()
